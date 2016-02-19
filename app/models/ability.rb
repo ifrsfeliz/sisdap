223997710dda49db2c0bb7a6aa5fcaec8b3f8525
Mauricio N. Ferreira <contato@ferreiramauricio.com>
2016-02-19 15:45:57 -0200

    Arrumado permissões para almoxarifado não aprovar sua propria entrada

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,14 +5,17 @@ class Ability
     user ||= User.new # guest user
 
     if user.role? :admin
+      can :read, :all
       can :manage, :all
     elsif user.role? :licitacao
+      can :read, :all
       can :manage, Uasg
       can :manage, Supplier
       can :manage, RequestLog
     elsif user.role? :almoxarifado
+      can :read, :all
       can :manage, StockroomItem
-      can :manage, StockroomMovimentation
+      can :create, StockroomMovimentation
     else #solicitante
       can :read, :all
     end
